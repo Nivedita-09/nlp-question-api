5e3ab1ca504f950d71c3ca4d64c07c02c37b0209
Weather forecast with Alan AI <niveditamahato2001@gmail.com>
2023-06-12 18:08:14 +0530

    Added Initial Files

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import pickle 
+from helper import query_point_creator
+
+
+
+model = pickle.load(open('model.pkl','rb'))
+app = Flask(__name__)
+
+@app.route("/", methods = ['POST'])
+def Main():
+    q1= request.form.get("Question1")
+    q2 = request.form.get("Question2")
+    query = query_point_creator(q1,q2)
+    result = model.predict(query)[0]
+    if result:
+        return 'Duplicate'
+    else:
+        return 'Not Duplicate'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
